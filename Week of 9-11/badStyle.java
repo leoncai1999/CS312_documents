22da1c7599c1cf842fdc139c9d7464a8f9186bb0
aish12 <ashashidhar@utexas.edu>
2017-09-11 17:08:15 -0500

    Update badStyle.java

--- Week of 9-11/badStyle.java
+++ Week of 9-11/badStyle.java
@@ -23,10 +23,4 @@ System.out.println(x-y);
 }
 
 
-1. Class names should always be capitalized
-2. Method names should not start with a capital letter
-3. Name your methods and variables something that makes sense
-4. Indent your code
-5. Make sure you limit the scope of your variables-- the variable “a” should be put inside the printNumber method.
-6. What is 3.14? It should be a constant named pi.
-7. There are absolutely no comments for this class and there should be.
+
