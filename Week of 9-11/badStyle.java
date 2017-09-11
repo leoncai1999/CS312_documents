fe7346aead4535c0dee6418c91bc86db2497acd5
aish12 <ashashidhar@utexas.edu>
2017-09-11 00:51:45 -0500

    Add files via upload

---
+++ Week of 9-11/badStyle.java
@@ -0,0 +1,32 @@
+public class hello {
+
+	int a=5;
+	public static void main(String[] args) {
+
+	}
+
+	public static void Add(int x, int y) {
+		System.out.println(x+y);
+	}
+
+public static void lksdfjlhgskdjfg(int x, int y) {
+System.out.println(x-y);
+}
+	public static void printNumber {
+		System.out.println(a);
+	}
+
+	public static void printCircleArea(int diameter) {
+		System.out.println(3.14*diameter);
+	}
+
+}
+
+
+1. Class names should always be capitalized
+2. Method names should not start with a capital letter
+3. Name your methods and variables something that makes sense
+4. Indent your code
+5. Make sure you limit the scope of your variables-- the variable “a” should be put inside the printNumber method.
+6. What is 3.14? It should be a constant named pi.
+7. There are absolutely no comments for this class and there should be.
